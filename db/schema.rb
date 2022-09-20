7d1063d48921da9e9f9af68e0f1f4ec2259cb487
KhoaVo <khoavodang1451997@gmail.com>
2022-09-20 23:09:24 +0700

    password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_164636) do
+ActiveRecord::Schema.define(version: 2022_09_20_103316) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2022_09_19_164636) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_foreign_key "microposts", "users"
